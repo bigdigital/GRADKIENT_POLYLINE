75ad4a5ef7403e2e56b24bcdad3beab544d0ddbb
bigdigital <bigdigital.kov@gmail.com>
2022-12-15 12:28:59 +0200

    //Example 4         //issue: Although the addPoint function is not documented, it somehow works.         // But for me it is expected it should put only dots to the canvas, but instead it outputs a dots connected with a lines         // , see https://i.imgur.com/FsDutRi.png

--- page/gtr3-pro/home/index.page.js
+++ page/gtr3-pro/home/index.page.js
@@ -5,8 +5,10 @@ Page({
     build() {
         logger.debug('page build invoked')
 
-        //Example 3
-        //issue: If create two widgets, the second widget will replace all properties for the first (color, lineData, etc), see https://i.imgur.com/j3eDIek.png
+        //Example 4
+        //issue: Although the addPoint function is not documented, it somehow works.
+        // But for me it is expected it should put only dots to the canvas, but instead it outputs a dots connected with a lines
+        // , see https://i.imgur.com/FsDutRi.png
 
         const Colors = {
             white: 0xffffff,
@@ -23,10 +25,10 @@ Page({
             color: Colors.white,
         });
 
-        let lineDataList = [
-            {x: 0, y: 50},
-            {x: 480, y: 50},
-        ];
+        // let lineDataList = [
+        //     {x: 0, y: 50},
+        //     {x: 480, y: 50},
+        // ];
 
         let lineDataList2 = [
             { x: 0, y: px(200) },
@@ -36,31 +38,31 @@ Page({
             { x: px(400), y: px(200) }
         ];
 
-        const polyline = hmUI.createWidget(hmUI.widget.GRADKIENT_POLYLINE, {
-            x: 0,
-            y: 0,
-            w: DEVICE_WIDTH,
-            h: 100,
-            line_color: Colors.red,
-            line_width: 1
-        })
-        polyline.clear()
-        polyline.addLine({
-            data: lineDataList,
-            count: lineDataList.length
-        })
+        // const polyline = hmUI.createWidget(hmUI.widget.GRADKIENT_POLYLINE, {
+        //     x: 0,
+        //     y: 0,
+        //     w: DEVICE_WIDTH,
+        //     h: 100,
+        //     line_color: Colors.red,
+        //     line_width: 1
+        // })
+        // polyline.clear()
+        // polyline.addLine({
+        //     data: lineDataList,
+        //     count: lineDataList.length
+        // })
 
         //move down the second widget
         const polyline2 = hmUI.createWidget(hmUI.widget.GRADKIENT_POLYLINE, {
             x: 0,
-            y: 100,
+            y: 0,
             w: DEVICE_WIDTH,
             h: 100,
             line_color: Colors.blue,
-            line_width: 4
+            line_width: 10
         })
         polyline2.clear()
-        polyline2.addLine({
+        polyline2.addPoint({
             data: lineDataList2,
             count: lineDataList2.length
         })
